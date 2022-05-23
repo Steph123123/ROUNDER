14e88a0904b18edb3005f51e6b65cdbcf50ea19a
Steph123123 <steph13am@gmail.com>
2022-05-23 16:28:55 +0000

    por las vias del ferrocarril

--- migrations/versions/0e83ecb5dbfb_.py
+++ migrations/versions/0e83ecb5dbfb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: fb5a96caccec
+Revision ID: 0e83ecb5dbfb
 Revises: 
-Create Date: 2022-05-22 14:59:08.389186
+Create Date: 2022-05-23 15:41:55.927873
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fb5a96caccec'
+revision = '0e83ecb5dbfb'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/pages/aboutus.js
+++ src/front/js/pages/aboutus.js
@@ -8,21 +8,31 @@ export const Aboutus = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div>
-    <div className="jumboabout profcont container  mt-5">
-      <div class="mt-4 p-5   rounded">
-        <h1 className="abouth1 container text-center mt-5">About Rex-Rounder</h1>
-        <h2 cassName="aboouth2">Rex-Rounder founder</h2>
-        <h3>Stephania Alvarez</h3>
-        <p>
-
-I am a veterinarian, getting to know new fields of work where I have found new goals, different ways of working and which have greatly enriched me in the world of programming and web development.
-
-this project like others, was born from my passion for motorcycles and in it I put into practice the knowledge acquired in the academai 4 geeks during these last months
-
-I hope you know the project and that you like it</p>
+    <div className="container">
+      <div className="jumboabout profcont container  mt-5">
+        <div class="mt-4 p-5   rounded">
+          <h1 className="abouth1 container text-center mt-5">
+            About REX-ROUNDER
+          </h1>
+         
+          <div className="d-flex">
+             <video className="videoaout"
+            src="https://rr5---sn-h5q7knel.c.drive.google.com/videoplayback?expire=1653336089&ei=2a-LYsejAsnWhwb8ppe4CQ&ip=83.41.213.31&cp=QVRKVURfVVBWSlhPOmN5X3J5M0ZZdG1hSkRqalV3WDN0YldwbVZzd1dPU2o2Y1VoZTZSeWhlRjU&id=288c5ce80f9a64d0&itag=22&source=webdrive&requiressl=yes&mh=RA&mm=32&mn=sn-h5q7knel&ms=su&mv=m&mvi=5&pl=16&ttl=transient&susc=dr&driveid=1zsTqSkS867K6WvvHFXli-z5wGyDRPK05&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=18.459&lmt=1631644745365841&mt=1653321392&subapp=DRIVE_WEB_FILE_VIEWER&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIhALx06SsFz-vlDG6MonGMZd6Y1l_zcXmZNN7zsYXBq6LrAiBRGv36RSKOSM4DViexaDXcGMqCFTxaF7-DTmayIz_jcA==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAMDWAKAwicyHqBQEObSvtO2OS0iduC6tnj8WdAt1fDYzAiEA2JScc8AimjAlRNx3R9hJL-Mzh0cHrGd4Iw2cZG4oJs0=&cpn=eQMwr2Kgni_FHjbt&c=WEB_EMBEDDED_PLAYER&cver=1.20220517.01.01"
+            controls
+          >
+            {" "}
+          </video>
+            <p className="pabout">
+              I am Stephania Alvarez, a veterinarian getting into new work fields where I
+              have found new goals, different ways of working and which have
+              greatly enriched me in the world of programming and web
+              development. This project <strong>like others</strong>, was born from my passion
+              for motorcycles and in it I put into practice the knowledge
+              acquired at the 4 Geeks Academy during these last months I hope you like it and enjoy
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
